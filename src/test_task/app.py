da41f36018ecfcbefb3d116337ab1f4605ed45c9
unknown <dartmol21@mail.ru>
2023-10-05 17:49:37 +0300

    close up to finish

--- src/test_task/app.py
+++ src/test_task/app.py
@@ -4,6 +4,8 @@ import dash_mantine_components as dmc
 from dash.exceptions import PreventUpdate
 import plotly.express as px
 from database.database import start_db
+import pandas as pd
+
 
 CARD_STYLE = dict(withBorder=True,
                   shadow="sm",
@@ -17,15 +19,55 @@ class EncostDash(DashProxy):
         super().__init__(transforms=[ServersideOutputTransform(),
                                      MultiplexerTransform()], **kwargs)
 
-percent_reasons, reason_option, options_for_dropdown, all_records_db = start_db()
+percent_reasons, reason_option, options_for_dropdown, all_records_db, color_map, unselected_map = start_db()
 
 app = EncostDash(name=__name__)
-pie_chart = px.pie(all_records_db,'reason', hole=.2)
+pie_chart = px.pie(all_records_db,'reason', hole=.2, color="reason", color_discrete_map  = color_map)
 pie_chart.update_layout(
     margin=dict(l=20, r=20, t=0, b=170),
 )
 
-histogram = px.bar(all_records_db, x='state_begin', hover_name='reason')
+fig_gant = px.timeline(
+    all_records_db, 
+    x_start="state_begin", 
+    x_end="state_end", 
+    y="endpoint_name", 
+    color='reason', 
+    color_discrete_map = color_map,
+    custom_data=[
+        'state',
+        'reason',
+        'state_begin',
+        'duration_min',
+        'calendar_day',
+        'period_name',
+        'operator'
+    ]
+)
+
+fig_gant.update_traces(
+    hoverlabel=dict(
+        bgcolor = ['white']
+    ),
+    hovertemplate="<br>".join([
+        'Состояние - <b>%{customdata[0]}</b>',
+        'Причина - <b>%{customdata[1]}</b>',
+        'Начало - <b>%{customdata[2]| %H:%M:%S}</b> (%{customdata[2]|%d.%m})',
+        'Длительность - <b>%{customdata[3]:,.2f}</b> мин.<br><br>',
+        'Сменный день - <b>%{customdata[4]}</b>',
+        'Смена - <b>%{customdata[5]}</b>',
+        'Оператор - <b>%{customdata[6]}</b>'
+    ])
+)
+
+fig_gant.update_layout(
+    title_text='График состояний',
+    xaxis_title="",
+    yaxis_title="",
+    showlegend=False
+)
+
+
 
 
 
@@ -35,15 +77,8 @@ def get_layout():
             dmc.Grid([
                 dmc.Col([
                     dmc.Card([
-                            dmc.TextInput(label='Введите что-нибудь', id='input'),
                             dcc.Dropdown(
-                                id="my_dropdown",
-                                # options=[
-                                #     {"label": "1", "value": "1"},
-                                #     {"label": "2", "value": "2"},
-                                #     {"label": "3", "value": "3"},
-                                #     {"label": "4", "value": "4"}
-                                # ],
+                                id="filter_dropdown",
                                 options=options_for_dropdown,
                                 multi=True,
                                 clearable=True,
@@ -54,28 +89,13 @@ def get_layout():
                                     "marginBottom": "15px"
                                 }
                             ),
-                            dmc.Button('Первая кнопка', id='button1'),
-                            dmc.Button('Вторая кнопка', id='button2'),
+                            dmc.Button('Фильтровать', id='filter_button'),
                             html.Div(id='output')
                         ],
                         **CARD_STYLE)
                 ], span=6),
                 dmc.Col([
                     dmc.Card([
-                            # html.Div('Верхняя правая карточка'),
-                            # html div from wxmaple 
-                                # html.Div([
-                                #     dcc.Graph(id="graph"),
-                                #     dcc.Dropdown(id='names',
-                                #         options=reason_option,
-                                #         value='day', clearable=False
-                                #     ),
-                                #     dcc.Dropdown(id='values',
-                                #         options=reason_option,
-                                #         value='total_bill', clearable=False
-                                #     ),
-                                # ])
-                            # html div from wxmaple 
                             html.Div(
                                 dcc.Graph(
                                     figure=pie_chart
@@ -89,10 +109,12 @@ def get_layout():
                 ], span=6),
                 dmc.Col([
                     dmc.Card([
-                        html.Div('График состояний'),
                         html.Div(
                             dcc.Graph(
-                                figure=histogram
+                                id="time_line_graph",
+                                figure=fig_gant,
+                                
+                                
                             )
                         )
                         
@@ -107,20 +129,6 @@ def get_layout():
 app.layout = get_layout()
 
 
-@app.callback(
-    Output('output', 'children'),
-    State('input', 'value'),
-    Input('button1', 'n_clicks'),
-    prevent_initial_call=True,
-)
-def update_div1(
-    value,
-    click
-):
-    if click is None:
-        raise PreventUpdate
-
-    return f'Первая кнопка нажата, данные: {value}'
 
 
 """app calback from example"""
@@ -150,21 +158,62 @@ def update_div1(
 #     return piechart
 
 
+
+# @app.callback(
+#     Output(component_id='time_line_graph', component_property='figure'),
+#     [Input(component_id='filter_dropdown', component_property='value')]
+# )
+
+# def highlight_part_graph(filter_dropdown):
+
+#     piechart = px.pie(
+#         all_records_db,
+#         'reason',
+#         hole=.2
+#     )
+
+#     return piechart
+
+
 @app.callback(
-    Output('output', 'children'),
-    State('input', 'value'),
-    Input('button2', 'n_clicks'),
+    Output(component_id='time_line_graph', component_property='figure'),
+    State(component_id='filter_dropdown', component_property='value'),
+    Input(component_id='filter_button', component_property='n_clicks'),
     prevent_initial_call=True,
 )
-def update_div2(
-    value,
-    click
-):
+
+def show_value(value, click):
     if click is None:
         raise PreventUpdate
+    
+    changed_unselected_map = unselected_map
+    print(value)
+    print("\n\n")
+    print(changed_unselected_map)
+    print("\n\n")
+    print(unselected_map)
+    for v in value:
+        if v in changed_unselected_map:
+            changed_unselected_map[v] = color_map[v]
 
-    return f'Вторая кнопка нажата, данные: {value}'
-
+    fig_gant = px.timeline(
+    all_records_db, 
+    x_start="state_begin", 
+    x_end="state_end", 
+    y="endpoint_name", 
+    color='reason', 
+    color_discrete_map = changed_unselected_map,
+    custom_data=[
+        'state',
+        'reason',
+        'state_begin',
+        'duration_min',
+        'calendar_day',
+        'period_name',
+        'operator'
+    ]
+)
+    return fig_gant
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- src/test_task/database/database.py
+++ src/test_task/database/database.py
@@ -5,6 +5,8 @@ def start_db():
     percent_reasons = {}
     reason_option = []
     options_for_dropdown = []
+    color_map = {}
+    unselected_map = {}
 
     con = sqlite3.connect("testDB.db")
     all_records_db = ps.read_sql("select * from sources", con)
@@ -14,28 +16,16 @@ def start_db():
 
 
     all_state = ps.read_sql("select distinct sources.state from sources", con)
-    print(all_state)
-    print('\n\n')
 
-
-    all_reasons = ps.read_sql("select distinct sources.reason from sources", con)
-    print(all_reasons)
-
-    print("\n\n")
+    all_reasons = ps.read_sql("select distinct sources.reason, sources.color from sources", con)
+   
 
     for index, row in all_reasons.iterrows():
         count_reson = ps.read_sql(f"select count(sources.reason) as county from sources where sources.reason = ?", con, params=(row['reason'], ))
         reason_option.append(row['reason'])
         percent_reasons[row['reason']] = (count_reson['county'][0] / count_all_records) * 100
         options_for_dropdown.append({"label": row['reason'], "value": row['reason']})
-        print(f"{row['reason']}: {count_reson['county'][0]}, процент: {percent_reasons[row['reason']]}")
+        color_map[row['reason']] = row['color']
+        unselected_map[row['reason']] = "#696969"
 
-    return percent_reasons, reason_option, options_for_dropdown, all_records_db
-
-# def prepare_data():
-#     con = sqlite3.connect("../testDB.db")
-#     all_records_db = ps.read_sql("select sources.client_name from sources", con)
-#     return all_records_db
-
-# data = prepare_data()
-# print(data['client_name'])
+    return percent_reasons, reason_option, options_for_dropdown, all_records_db, color_map, unselected_map
